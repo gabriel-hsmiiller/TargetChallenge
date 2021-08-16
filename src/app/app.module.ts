daefe4b5ff922e00e49c2a17cddc63d90f907602
Gabriel Miiller <gbmiiller@hotmail.com>
2021-08-16 15:13:44 -0300

    Creating base componetns

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RightComponent } from './components/right/right.component';
+import { LeftComponent } from './components/left/left.component';
+import { MiddleComponent } from './components/middle/middle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RightComponent,
+    LeftComponent,
+    MiddleComponent
   ],
   imports: [
     BrowserModule
